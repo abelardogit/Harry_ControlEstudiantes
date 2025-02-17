15d998e330c2d9d6bab611d671653b462965e50b
harryvillanueva <harryvillanueva0814@gmail.com>
2025-02-17 19:07:01 +0100

    subiendo funcionalidad agregar alumno

--- src/alumno/AdministrarAlumno.java
+++ src/alumno/AdministrarAlumno.java
@@ -1,4 +1,47 @@
 package alumno;
 
+import input.Input;
+import output.Output;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class AdministrarAlumno {
+
+    public static List<Alumno> listaAlumnos;
+    public static Output output ;
+    public static Input input ;
+
+
+    public AdministrarAlumno() {
+        listaAlumnos = new ArrayList();
+        output = new Output();
+        input = new Input();
+
+    }
+
+
+    public  void agregarAlumno(){
+        output.mostrarMensajeDni();
+        String dniAlumno = input.getPalabra();
+        output.mostrarMensajeNombre();
+        String nombre = input.getPalabra();
+        output.mostrarMensajeEdad();
+        int edad = input.getInt();
+        output.mostrarMensajeFechaNac();
+        String fechaNac = input.getPalabra();
+        output.mostrarMensajeAsignatura();
+        String asignatura1 = input.getPalabra();
+        output.mostrarMensajeNota();
+        double nota1 = input.getDouble();
+        output.mostrarMensajeAsignatura();
+        String asignatura2 = input.getPalabra();
+        output.mostrarMensajeNota();
+        double nota2 = input.getDouble();
+
+        listaAlumnos.add(new Alumno(dniAlumno,nombre,edad,fechaNac,asignatura1,nota1,asignatura2,nota2));
+
+
+    }
+
 }
--- src/output/Output.java
+++ src/output/Output.java
@@ -4,9 +4,37 @@ public class Output {
 
     public void mostrarMenu(){
         System.out.println("""
-                
-                
-                
+                Ingrese la opcion que desea realizar
+                1.Agregar Alumno
+                2.Listar Alumno
+                3.Modificar Alumno
+                4.Eliminar Alumno
+                5.Salir                
                 """);
+    }
+
+    public void mostrarMensajeDni() {
+        System.out.println("Ingrese numero de DNI");
+    }
+
+    public void mostrarMensajeNombre() {
+        System.out.println("ingrese Nombre");
+    }
+
+    public void mostrarMensajeEdad() {
+        System.out.println("ingrese edad");
+    }
+
+    public void mostrarMensajeFechaNac() {
+        System.out.println("ingrese fecha de nacimiento");
+    }
+
+    public void mostrarMensajeAsignatura() {
+        System.out.println("ingrese Asignatura");
+    }
+
+
+    public void mostrarMensajeNota() {
+        System.out.println("ingrese Nota");
     }
 }
